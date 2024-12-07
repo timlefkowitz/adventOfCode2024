913841513fc2df6c728342389c83710d5521b0d8
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2024-12-06 19:26:34 -0600

    Update Day two.scala

--- Day two.scala
+++ Day two.scala
@@ -1,1 +1,21 @@
+import java.io.File
+import scala.io.Source
 
+object main {
+  def main(args: Array[String]): Unit = {
+    try {
+
+    println(s"Current working dir : ${new java.io.File(".").getCanonicalPath}")
+    val filePath = "data.txt"
+
+     
+    println(s"")
+  } catch {
+    case e: Exception =>
+      println(s"Error: ${e.getMessage}")
+
+    //println(s"Smallest pair: ($smallestLeft, $smallestRight)")
+    //println(s"Distance: $distance")
+  }
+  }
+}
